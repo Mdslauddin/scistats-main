64d0c0d86acae5fde35d5269dd503e1e45bcbcf4
Mdslauddin <mdslauddin285@gamil.com>
2023-02-03 19:18:06 +0530

    first commit arange the dir structure

---
+++ scistats/descriptive/tests/test.py
@@ -0,0 +1,42 @@
+""" 
+Descriptive Statistics and Visualization 
+
+Categories:
+1.) Managing Data: Data import and export grouping variables
+2.) Descriptive statistics : Numerical summarises and associated measures
+3.) statistical visualizaiton : view data patterns and trens 
+
+
+# Central Tendency and Dispersion
+- geomean 
+- harmmean 
+- trimean 
+- kurtosis
+- moment
+- skewness
+
+
+"""
+
+__all__ = ['geomean','harmmean','trimean','kurtosis','moment','skewness']
+
+def geomean():
+    pass
+
+def harmmean():
+    pass
+
+def trimean():
+    pass
+
+def kurtosis():
+    pass
+
+def moment():
+    pass
+
+def skewness():
+    pass 
+
+
+
