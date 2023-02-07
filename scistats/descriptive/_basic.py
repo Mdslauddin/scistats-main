e764741db636ba9b20d92aff5a702aaa9d9d52d6
Mdslauddin <mdslauddin285@gamil.com>
2023-02-07 09:42:42 +0530

    version file

---
+++ scistats/descriptive/_basic.py
@@ -0,0 +1,38 @@
+def harmmean():
+    """
+    Return the harmonic mean of data.
+
+    The harmonic mean, sometimes called the subcontrary mean, is the
+    reciprocal of the arithmetic mean of the reciprocals of the data,
+    and is often appropriate when averaging quantities which are rates
+    or ratios, for example speeds. Example:
+
+    Suppose an investor purchases an equal value of shares in each of
+    three companies, with P/E (price/earning) ratios of 2.5, 3 and 10.
+    What is the average P/E ratio for the investor's portfolio?
+
+    >>> harmonic_mean([2.5, 3, 10])  # For an equal investment portfolio.
+    3.6
+
+    Using the arithmetic mean would give an average of about 5.167, which
+    is too high.
+
+    If ``data`` is empty, or any element is less than zero,
+    ``harmonic_mean`` will raise ``StatisticsError``.
+    """
+    return 0.0 
+
+def geomean():
+    """
+    Convert data to floats and compute the geometric mean.
+
+    Raises a StatisticsError if the input dataset is empty,
+    if it contains a zero, or if it contains a negative value.
+
+    No special efforts are made to achieve exact results.
+    (However, this may change in the future.)
+
+    >>> round(geometric_mean([54, 24, 36]), 9)
+    36.0
+    """
+    return 0.0
---
+++ scistats/version.py
@@ -0,0 +1,8 @@
+import scistats
+
+__all__ = [dir(scistats)]
+__version__ = '0.0.1'
+__author__ = 'mdslauddin'
+__email__ = 'mdslauddin285@gmail.com'
+__name__ = 'scistats'
+__package__ = 'scistats'
